70b5fa25cf5e3dea175d81dfe6a82456fc074a67
Justin Tooley <jstooley823@gmail.com>
2018-02-15 21:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver (name){
   return drivers.push(name)
 }
+function destructivelyPrependDriver (name){
+  return drivers.shift(name)
+}
